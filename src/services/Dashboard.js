912fb45528c269a51f7862d54a81ac20da161186
priyanjal1234 <priyanjal362@gmail.com>
2025-03-18 10:45:23 +0530

    Update Dashboard.js

--- src/services/Dashboard.js
+++ src/services/Dashboard.js
@@ -3,7 +3,7 @@ import api from "./api";
 class DashboardService {
   constructor() {
     this.api = api;
-    this.baseUrl = "http://localhost:3000/api/admin";
+    this.baseUrl = "https://skillify-lms.xyz/api/admin";
   }
 
   async getDashboardDetails() {
