e276ba749af3280e7ae5ca48a7c39e06b88fc348
priyanjal1234 <priyanjal362@gmail.com>
2025-03-18 10:44:54 +0530

    Update Student.js

--- src/services/Student.js
+++ src/services/Student.js
@@ -3,7 +3,7 @@ import api from "./api";
 class StudentService {
   constructor() {
     this.api = api;
-    this.baseUrl = "http://localhost:3000/api/admin";
+    this.baseUrl = "https://skillify-lms.xyz/api/admin";
   }
 
   async getStudentDetails(page = 1, limit = 5) {
